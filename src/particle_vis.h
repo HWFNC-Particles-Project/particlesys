59b69baa7a686377ca306a4951181dd8c3642f78
Benjamin <benj@gmx.net>
2014-05-06 14:31:06 +0100

    some configuration and output for performance meausrements

--- src/particle_vis.h
+++ src/particle_vis.h
@@ -1,7 +1,7 @@
 #ifndef PARTICLE_DRAW_H
 #define PARTICLE_DRAW_H
 
-#include <particle_array.h>
+#include "particle_array.h"
 
 int particle_vis_init();
 void particle_vis_draw(particle_array *particles);
